08797af9c2bbfeb8c5beba7411637245ec0f6efa
soozzi0303 <soozzichung@gmail.com>
2024-05-29 01:05:12 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,7 +20,8 @@ video.setAttribute('playsinline', '')
 const constraints = {
     audio: false,
     video: {
-        width: 100px,
+        width: { min: 640, ideal: 1280, max: 1920 },
+        height: { min: 480, ideal: 720, max: 1080 },
         facingMode: { 
             ideal: "environment" 
         }
